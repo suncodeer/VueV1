a5b39e7b932533a35620bebd1d8897810ee9ad09
user001 <alonesama@163.com>
2017-10-03 20:45:26 +0800

    first commit

---
+++ src/components/Greet.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="Greet">
+		<h6> I am the component from greet.vue file </h6>
+		<ol>
+			<li>List One</li>
+			<li>List Two</li>
+			<li>{{message}}</li>
+		</ol>
+	</div>
+</template>
+
+<script >
+	module.exports = {
+		name: 'Greet',
+		data: function () {
+			return {
+				'message': 'Hello single Vue File world'
+			}
+		}
+	}
+</script>
+<style>
+
+	ul h6 {
+		padding: 0px;
+		margin: 0px
+	}
+</style>
